bec832c4ea62ffa29b9646e5a48b357ecd555344
Jafeth Daniel Gamboa Baas <jafethgamboabaas@gmail.com>
2024-12-06 17:48:43 -0600

    Actualizar la configuración de la aplicación y mejorar la gestión de dimensiones de pantalla

--- appdoctor/lib/utils/config.dart
+++ appdoctor/lib/utils/config.dart
@@ -3,15 +3,39 @@ import 'package:flutter/material.dart'; //Importamos el paquete material
 
 class Config {
   static MediaQueryData? mediaQueryData; //Datos de la consulta de medios
-  static double? screenWidth; //Ancho de la pantalla
-  static double? screenHeight; //Altura de la pantalla
+  static double? anchodepantalla; //Ancho de la pantalla
+  static double? altodepantalla; //Altura de la pantalla
 
   static void init(BuildContext context) {
     mediaQueryData =
         MediaQuery.of(context); //Obtenemos los datos de la consulta de medios
-    screenWidth =
+    anchodepantalla =
         mediaQueryData!.size.width; //Obtenemos el ancho de la pantalla
-    screenHeight =
+    altodepantalla =
         mediaQueryData!.size.height; //Obtenemos la altura de la pantalla
   }
+
+  static get anchodePantalla {
+    return anchodepantalla!; //Retornamos el ancho de la pantalla
+  }
+
+  static get altodePantalla {
+    return altodepantalla!; //Retornamos la altura de la pantalla
+  }
+  //definimos el espacio de la altura de la pantalla
+  // ignore: constant_identifier_names
+  static const espacioPequeño = SizedBox(height: 25,);
+  static final espaciomediano = SizedBox(height: altodepantalla != null ? altodepantalla! * 0.05 : 0);
+  static final espaciogrande = SizedBox(height: altodepantalla != null ? altodepantalla! * 0.08 : 0);
+
+    //forma del texto bordes redondeados
+    static const OutlinedBorder = OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(8.0))
+        );
+
+    static const focusedBorder = OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        borderSide: BorderSide(color: Colors.blue, width: 2.0)
+        );
+
 }
