bbd90a5f7345e268636391f22ff706bfdfa1b4de
Jafeth Daniel Gamboa Baas <jafethgamboabaas@gmail.com>
2024-12-06 19:08:14 -0600

    Actualizar el texto de prueba en el cuerpo de la página principal

--- appdoctor/lib/main.dart
+++ appdoctor/lib/main.dart
@@ -56,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Este es un texto de prueba para el body:',
             ),
             Text(
               '$_counter',
