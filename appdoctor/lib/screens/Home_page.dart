1cb6b097665c90deb5b43f6ebee14be6a6f6837d
Jafeth Daniel Gamboa Baas <jafethgamboabaas@gmail.com>
2024-12-12 00:32:39 -0600

    Agregar imagen de usuario y mejorar diseño en HomePage

--- appdoctor/lib/screens/Home_page.dart
+++ appdoctor/lib/screens/Home_page.dart
@@ -1,3 +1,4 @@
+import 'package:appdoctor/utils/config.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,8 +15,53 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     // Construcción de la página de inicio
-    return Center(
-      child: Text('home page'), // Texto de la página de inicio
-    ); // Devuelve el centro de la página de inicio
+    Config.init(context); // Inicializa la configuración de la aplicación
+    return Scaffold(
+      // Devuelve la estructura de la página de inicio
+      body: Padding(
+        // Padding es un widget que permite agregar relleno a un widget hijo
+        padding: const EdgeInsets.symmetric(
+            // edgeInsets.symmetric crea un padding simétrico
+            horizontal: 15,
+            vertical: 15
+            // establece el relleno horizontal y vertical
+            ),
+        child: SafeArea(
+          // SafeArea es un widget que permite evitar que los elementos se superpongan en el área segura
+          child: Column(
+            // Column es un widget que permite alinear los elementos en una columna
+            mainAxisAlignment: MainAxisAlignment
+                .start, //esto alinea los elementos en la parte superior
+            crossAxisAlignment: CrossAxisAlignment
+                .start, //esto alinea los elementos en la parte izquierda
+            children: <Widget>[
+              // Lista de widgets
+              Row(
+                mainAxisAlignment: MainAxisAlignment
+                    .spaceBetween, // Alineación principal entre los elementos
+                children: <Widget>[
+                  // Lista de widgets
+                  Text(
+                    // Texto de bienvenida
+                    'Amanda', // Nombre de usuario
+                    style: TextStyle(
+                      // Estilo del texto
+                      fontSize: 24, // Tamaño de la fuente
+                      fontWeight: FontWeight.bold, // Peso de la fuente
+                    ),
+                  ),
+                  CircleAvatar(
+                    // Avatar circular
+                    radius: 30, // Radio del avatar
+                    backgroundImage: AssetImage(
+                        'assets/amanda.jpg'), // Imagen de fondo del avatar
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
