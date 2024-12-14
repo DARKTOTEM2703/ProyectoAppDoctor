c271e127af4f5a55d897cb4d56398608b02402e4
Jafeth Daniel Gamboa Baas <jafethgamboabaas@gmail.com>
2024-12-14 03:03:25 -0600

    Agregar botón de favorito y refactorizar la tarjeta del doctor en DoctorDetails

--- appdoctor/lib/screens/doctor_details.dart
+++ appdoctor/lib/screens/doctor_details.dart
@@ -1,3 +1,4 @@
+import 'package:appdoctor/utils/config.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:appdoctor/components/custom_appbar.dart';
@@ -19,7 +20,45 @@ class _DoctorDetails extends State<DoctorDetails> {
       appBar: CustomAppBar(
         appTitle: 'Doctor Details',
         icon: const Icon(Icons.arrow_back_ios),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                isFavourite ? Icons.favorite_rounded : Icons.favorite_outline,
+                color: Colors.red,
+              ))
+        ],
       ),
+      body: SafeArea(
+          child: Column(
+        children: <Widget>[
+          //aqui construimos la tarjeta del doctor
+          InformacionDoctor(),
+        ],
+      )),
     );
+  }
+}
+
+class InformacionDoctor extends StatelessWidget {
+  const InformacionDoctor({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Config.init(context);
+    return Container(
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            const CircleAvatar(
+              radius: 60,
+              backgroundImage: AssetImage('assets/Doctor_2.jpg'),
+              backgroundColor: Colors.white,
+            ),
+            Config.espacioMediano,
+          ],
+        ));
   }
 }
