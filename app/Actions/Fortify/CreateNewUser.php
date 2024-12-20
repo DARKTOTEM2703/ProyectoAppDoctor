b3ccf371861814e2b61deb981670abd4015087e1
Jafeth Daniel Gamboa Baas <jafethgamboabaas@gmail.com>
2024-12-20 08:07:58 -0600

    Agregar lógica para crear registros de Doctor y UserDetails al registrar un nuevo usuario, diferenciando entre tipos de usuario.

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -1,23 +1,21 @@
 <?php
 
 namespace App\Actions\Fortify;
-
+use App\Models\Doctor;
 use App\Models\User;
+use App\Models\UserDetails;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Jetstream\Jetstream;
 
+// Este es el registro nuevo de usuario/doctor
+
 class CreateNewUser implements CreatesNewUsers
 {
     use PasswordValidationRules;
 
-    /**
-     * Validate and create a newly registered user.
-     *
-     * @param  array<string, string>  $input
-     */
-    public function create(array $input): User
+    public function create(array $input)
     {
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
@@ -26,10 +24,54 @@ class CreateNewUser implements CreatesNewUsers
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
-        return User::create([
+        $user = User::create([
             'name' => $input['name'],
             'email' => $input['email'],
+            'type'=>$input['type'], // añadimos el campo type para diferenciar entre usuario y doctor
             'password' => Hash::make($input['password']),
         ]);
+
+        /*Este if es para que si el tipo de usuario es doctor
+        se cree un registro en la tabla doctors
+        con los campos que se pueden asignar masivamente
+        que estan en el array $fillable de la clase Doctor
+         */
+
+        if($input['type'] == 'doctor'){
+            $doctorInfo = Doctor::create([
+            'name'=>$input['name'],
+            'email'=>$input['email'],
+            'type'=>$input['type'],
+            'password'=>Hash::make($input['password']),
+            ]);
+
+            /* Este if es para que si el tipo de usuario es user
+            se cree un registro en la tabla user_details
+            con los campos que se pueden asignar masivamente
+            que estan en el array $fillable de la clase UserDetails
+            */
+
+            if($input['type'] == 'doctor'){
+                $doctorInfo = Doctor::create([
+                    'doctor_id'=>$user->id,
+                    'status'=>'active',
+                ]);
+            }
+            /*este else if es para que si el tipo de usuario es user
+            se cree un registro en la tabla user_details
+            con los campos que se pueden asignar masivamente
+            que estan en el array $fillable de la clase UserDetails
+             */
+
+            else if($input['type'] == 'user'){
+                $userInfo = UserDetails::create([
+                    'user_id'=>$user->id,
+                    'status'=>'active',
+                ]);
+            }
+            //ahora, añadimos una entrada al registro form
+
+            return $user;
+        }
     }
 }
